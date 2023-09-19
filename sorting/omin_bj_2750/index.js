5bb84d0ad9053d9da7da2872f45fd82419408a75
Kevin Minseok Oh <oms3749@gmail.com>
2023-09-19 16:22:38 +0900

    omin 230919 sorting

--- sorting/omin_bj_2750/index.js
+++ sorting/omin_bj_2750/index.js
@@ -1,1 +1,8 @@
-console.log('2750번 문제');
+const fs = require('fs');
+const filepath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filepath).toString().trim().split('\n');
+
+input.shift();
+const arr = input.map(v => parseInt(v)).sort((a, b) => a - b);
+
+arr.forEach(v => console.log(v));
