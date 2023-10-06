ea5a39e1a4d0f20059feabc7fb82fa9566fe527d
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-06 17:44:46 +0900

    omin backtracking 231006

--- backtracking/omin_bj_1182/index.js
+++ backtracking/omin_bj_1182/index.js
@@ -5,36 +5,21 @@ const input = fs.readFileSync(filepath).toString().trim().split('\n');
 const [N, S] = input[0].split(' ').map(el => Number(el));
 const seq = input[1].split(' ').map(el => Number(el));
 
-solution();
+let answer = 0;
+combination([], 0);
 
-function solution() {
-  let answer = 0;
+console.log(answer);
 
-  for (let i = 1; i <= N; i++) {
-    const combinations = getCombinations(seq, i);
-    combinations.forEach(combination => {
-      const sum = combination.reduce((a, b) => a + b, 0);
-      if (sum === S) {
-        answer += 1;
-      }
-    });
+function combination(array, idx) {
+  const sum = array.reduce((a, b) => a + b, null);
+
+  if (sum === S) {
+    answer++;
   }
 
-  console.log(answer);
-}
-
-function getCombinations(arr, r) {
-  const result = [];
-
-  if (r === 1) return arr.map(el => [el]);
-
-  arr.forEach((fixed, idx, origin) => {
-    const rest = origin.slice(idx + 1);
-    const combinations = getCombinations(rest, r - 1);
-    const attached = combinations.map(combination => [fixed, ...combination]);
-
-    result.push(...attached);
-  });
-
-  return result;
+  for (let i = idx; i < N; i++) {
+    array.push(seq[i]);
+    combination(array, i + 1);
+    array.pop();
+  }
 }
