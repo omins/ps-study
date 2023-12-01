13556486e5c323ec5fa3c0cfce58f8265a242953
ohgus <anakin1108@naver.com>
2023-12-01 17:02:19 +0900

    oms dp 231201

---
+++ dp/oms_bj_1904/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
+const n = Number(input[0]);
+
+const dp = new Array(1000001).fill(0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+}
+
+console.log(dp[n]);
